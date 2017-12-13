f70a4dc44ef059089e838dda2efd19277af2132b
吴绪伟 <172498667@qq.com>
2017-12-13 19:24:18 +0800

    Fixed Bugs

--- frontend/views/house/house_list.php
+++ frontend/views/house/house_list.php
@@ -6,36 +6,15 @@ AppAsset::addCss($this,'@web/css/house_list.css?t='.time());
 $this->title = '全部楼盘';
 ?>
 <div id="main_page" >
-    <ul>
+    <ul id="house_list">
         <li class="house_item list-unstyled">
             <div class="house_info">
                 <div class="house_img"><img src="/images/demo.jpg" class="img-responsive" /></div>
                 <div class="house_summary">
                     <p class="house_name">新华联·奥莱悦府</p>
-                    <p class="house_area">80-120㎡|2-3室</p>
-                    <p class="house_unit_price">2-3室|80-120㎡</p>
-                    <p class="house_total_price">200万起</p>
-                </div>
-                <div class="house_return">
-                    <div class="return_img"><img src="/images/logo_small.png" /></div>
-                    <div class="return_des">
-                        <p>口袋返现</p>
-                        <p class="return_price">3-6万/套</p>
-                    </div>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="house_addresm">地址：上海市嘉定区江沙江路600弄</div>
-        </li>
-        <li class="house_item list-unstyled">
-            <div class="house_info">
-                <div class="house_img"><img src="/images/demo.jpg" /></div>
-                <div class="house_summary">
-                    <p class="house_name">新华联·奥莱悦府</p>
-                    <p class="house_area">80-120㎡|2-3室</p>
-                    <p class="house_unit_price">2-3室|80-120㎡</p>
-                    <p class="house_total_price">200万起</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1000元/㎡</p>
+                    <p class="house_total_price">总价：200万起</p>
                 </div>
                 <div class="house_return">
                     <div class="return_img"><img src="/images/logo_small.png" /></div>
@@ -49,5 +28,131 @@ $this->title = '全部楼盘';
             </div>
             <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
         </li>
+        <li class="house_item list-unstyled">
+            <div class="house_info">
+                <div class="house_img"><img src="http://fsxf.fangdd.com/xf/FtIImpUnyKWrMrK5iOchJxP_It76.png?imageMogr2/thumbnail/!240x168r/format/jpg" /></div>
+                <div class="house_summary">
+                    <p class="house_name">碧桂园金山星作</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1800元/㎡</p>
+                    <p class="house_total_price">总价：280万起</p>
+                </div>
+                <div class="house_return">
+                    <div class="return_img"><img src="/images/logo_small.png" /></div>
+                    <div class="return_des">
+                        <p>口袋返现</p>
+                        <p class="return_price">3-6万/套</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
+        </li>
+        <li class="house_item list-unstyled">
+            <div class="house_info">
+                <div class="house_img"><img src="http://fs.fangdd.com/image/kwWEhLMNmJ0EdQlaEcgqn-gn_Qg.jpg?imageMogr2/thumbnail/!240x168r/format/jpg" /></div>
+                <div class="house_summary">
+                    <p class="house_name">世茂佘山里</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1300元/㎡</p>
+                    <p class="house_total_price">总价：240万起</p>
+                </div>
+                <div class="house_return">
+                    <div class="return_img"><img src="/images/logo_small.png" /></div>
+                    <div class="return_des">
+                        <p>口袋返现</p>
+                        <p class="return_price">3-6万/套</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
+        </li>
+        <li class="house_item list-unstyled">
+            <div class="house_info">
+                <div class="house_img"><img src="/images/demo.jpg" /></div>
+                <div class="house_summary">
+                    <p class="house_name">新华联·奥莱悦府</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1800元/㎡</p>
+                    <p class="house_total_price">总价：280万起</p>
+                </div>
+                <div class="house_return">
+                    <div class="return_img"><img src="/images/logo_small.png" /></div>
+                    <div class="return_des">
+                        <p>口袋返现</p>
+                        <p class="return_price">3-6万/套</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
+        </li>
+        <li class="house_item list-unstyled">
+            <div class="house_info">
+                <div class="house_img"><img src="/images/demo.jpg" /></div>
+                <div class="house_summary">
+                    <p class="house_name">新华联·奥莱悦府</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1800元/㎡</p>
+                    <p class="house_total_price">总价：280万起</p>
+                </div>
+                <div class="house_return">
+                    <div class="return_img"><img src="/images/logo_small.png" /></div>
+                    <div class="return_des">
+                        <p>口袋返现</p>
+                        <p class="return_price">3-6万/套</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
+        </li><li class="house_item list-unstyled">
+            <div class="house_info">
+                <div class="house_img"><img src="/images/demo.jpg" /></div>
+                <div class="house_summary">
+                    <p class="house_name">新华联·奥莱悦府</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1800元/㎡</p>
+                    <p class="house_total_price">总价：280万起</p>
+                </div>
+                <div class="house_return">
+                    <div class="return_img"><img src="/images/logo_small.png" /></div>
+                    <div class="return_des">
+                        <p>口袋返现</p>
+                        <p class="return_price">3-6万/套</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
+        </li>
+        <li class="house_item list-unstyled">
+            <div class="house_info">
+                <div class="house_img"><img src="/images/demo.jpg" /></div>
+                <div class="house_summary">
+                    <p class="house_name">新华联·奥莱悦府</p>
+                    <p class="house_area">80-120㎡ | 2-3室</p>
+                    <p class="house_unit_price">单价：1800元/㎡</p>
+                    <p class="house_total_price">总价：280万起</p>
+                </div>
+                <div class="house_return">
+                    <div class="return_img"><img src="/images/logo_small.png" /></div>
+                    <div class="return_des">
+                        <p>口袋返现</p>
+                        <p class="return_price">3-6万/套</p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="house_address">地址：上海市嘉定区江沙江路600弄</div>
+        </li>
+
     </ul>
 </div>
