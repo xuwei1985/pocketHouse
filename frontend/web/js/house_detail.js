59da10e054dc3d3156d62848c9236faea7207fc0
vivi <172498667@qq.com>
2017-12-15 23:31:08 +0800

    00

--- frontend/web/js/house_detail.js
+++ frontend/web/js/house_detail.js
@@ -4,8 +4,48 @@ $(function () {
 
 function addEvent() {
     $(".prev_img").click(function(){
-        $("body").laod("/house_album");
+        openPhotoSwipe();
     });
 
-
 }
+
+var openPhotoSwipe = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: '../images/prev2.png',
+            w: 496,
+            h: 325,
+            title: '小区图1',
+        },
+        {
+            src: '../images/prev1.png',
+            w: 496,
+            h: 325,
+            title: '小区图2',
+
+        },
+        {
+            src: '../images/prev3.png',
+            w: 496,
+            h: 325,
+            title: '小区图3',
+        }
+    ];
+
+    // define options (if needed)
+    var options = {
+        // history & focus options are disabled on CodePen
+        history: false,
+        focus: false,
+
+        showAnimationDuration: true,
+        hideAnimationDuration: true
+
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
