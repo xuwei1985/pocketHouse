b214e6bae5e6a6324117698dbe69e157835dc505
吴绪伟 <172498667@qq.com>
2017-12-15 17:51:32 +0800

    Daily Update

--- frontend/controllers/HouseController.php
+++ frontend/controllers/HouseController.php
@@ -5,8 +5,14 @@ use yii\web\Controller;
 
 class HouseController extends Controller{
     public $layout="base";
+
     public function actionIndex()
     {
         return $this->render('house_list');
+    }
+
+    public function actionDetail()
+    {
+        return $this->render('house_detail');
     }
 }
